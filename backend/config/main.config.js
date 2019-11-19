cb0892923e583f39df6f8b546bba7982d02d3134
STM <stefa0826.14@gmail.com>
2019-11-19 15:21:48 -0500

    bd

---
+++ backend/config/main.config.js
@@ -0,0 +1,10 @@
+let config = {
+    USER_DB: "postgres",
+    HOST_DB: "localhost",
+    DATABASE: "universidad",
+    PASSWORD_DB: "Admin123",
+    PORT_DB: 5432,
+    SECRET_KEY: "MicLavEsEcreta123#!"
+  };
+  
+  module.exports = config;
