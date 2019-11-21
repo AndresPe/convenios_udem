1e411ae71dbeb68a6202410add90864e7ae8fd1e
STM <stefa0826.14@gmail.com>
2019-11-20 22:32:34 -0500

    registrar

--- backend/controllers/registroController.js
+++ backend/controllers/registroController.js
@@ -10,7 +10,9 @@ class Registro {
         let query = `INSERT INTO public.basic_info_user (
             id_user, name, last_name, address, telephone, sexo, email)
             VALUES ('${usuario.id_user}', '${usuario.name}', '${usuario.last_name}', 
-            '${usuario.address}', '${usuario.telephone}', ${usuario.sexo}, '${usuario.email}')`;
+            '${usuario.address}', '${usuario.telephone}', ${usuario.sexo}, '${usuario.email}');
+            INSERT INTO public.login(id_user, user_type, password) VALUES ('${usuario.id_user}', '${usuario.user_type}',
+            '${usuario.password}');`;
             let respuesta = await this.dbController.ejecutarSql(query);
             return respuesta;        
     }
