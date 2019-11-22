87c4f287181fcc88d1ef085246601d09e68edd8d
AndresPe <julipe.andres@gmail.com>
2019-11-21 20:23:49 -0500

    inntento de conectar el login del back con el front

---
+++ frontend/login.js
@@ -0,0 +1,16 @@
+import Axios from "axios";
+
+function login(){
+    let url = "http://localhost:3000/login/";
+    let usuario = document.getElementById("usuario").value;
+    let password = document.getElementById("clave").value;
+    let body = {usuario, password};
+    axios.post(url, body).then(response => {
+        let data = response.data;
+        if (data.estado == true) {
+            let informacion = data.informacion;
+            sessionStorage.setItem("token", informacion);
+            window.location = "formulario.html";
+        }
+    });
+}
